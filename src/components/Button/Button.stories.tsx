e793a10315fd9efc75ccb0f5d0857c85112dc004
Traximullin <66427615+Traximullin@users.noreply.github.com>
2022-12-19 21:02:22 +0300

    init storybook

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,9 @@
+import Button from "./Button";
+
+export default {
+  title: "Button",
+  component: Button,
+};
+
+// eslint-disable-next-line react/function-component-definition, react/react-in-jsx-scope
+export const Default: React.FC = () => <Button>Click</Button>;
